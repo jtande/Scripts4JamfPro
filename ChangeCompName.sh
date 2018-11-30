a382e7792c51e2f5cf2dc0f6a080fead0df9f69d
FOSSO <7119916+jtande@users.noreply.github.com>
2018-11-30 15:29:26 -0500

    Update ChangeCompName.sh

--- ChangeCompName.sh
+++ ChangeCompName.sh
@@ -1,9 +1,16 @@
-#! /usr/bin/bash
-#This script is used to reconfigure computername, hostname and localhostname
-# $1 is the new computername
-# $2 is the hostname
-# $3 is the localhostname
+#! /bin/bash
+#################################################################################################
+#
+# This script is used to reconfigure computername, hostname and localhostname
+# $1 is the new computername e.g NewImac 
+# $2 is the hostname e.g Imac.college.edu
+# $3 is the localhostname e.g NewImac
+# if using in Jamf Pro, omit sudo
 # synopsis ./ChangeCompName.sh YournewComputerName YourHostName YourLocalHostname
+#
+# By Jacob Fosso 11/30/2018
+#
+###############################################################################################
 
 sudo scutil --set ComputerName "$1"
 sudo scutil --set HostName "$2"
